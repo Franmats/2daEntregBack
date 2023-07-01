d5b43dba18f669d43229f4cd8e8340476eff842a
Franco Cuenca <francomats424@gmail.com>
2023-07-01 20:11:23 -0300

    correcciones

--- ProductManager.js
+++ ProductManager.js
@@ -8,10 +8,14 @@ class ProductManager {
     }
 
     getProducts = async() => {
-        if(fs.existsSync(__dirname + "/BD.json")) {
-            const read = await fs.promises.readFile(__dirname + "/BD.json","utf-8").then(e => JSON.parse(e))// con jsonparse lo convertimos a objeto para mostralo en consola si existen o actualizan los datos
-            console.log("Productos",read)
-        }else return []
+        if(fs.existsSync(__dirname + `/${this.#path}`)) {
+            const productoString = JSON.stringify(this.products)
+            fs.writeFileSync("BD.json", productoString)
+            const read = await fs.promises.readFile(__dirname + "/BD.json","utf-8").then(e => JSON.parse(e))// con jsonparse lo convertimos a objeto para mostralo en consola si existen o actualizan los datos 
+            return read 
+        }else {
+            fs.writeFileSync(`${this.#path}`,"[]")
+        }
     }
 
     getNextId = () => {
@@ -31,64 +35,99 @@ class ProductManager {
             code,
             stock
         }
-        //VALIDACION QUE TODOS LOS ESPACIOS DE LOS PRDOCUTOS ESTEN LLENOS QUE TENGA TODOS LOS PARAMETROS
-        const espaciosVacios = this.products.map(e => Object.values(e)).map(e => e.includes(undefined)).includes(true)
-        if (espaciosVacios == true) {
-            return console.log("Hay espacios vacios en la lista de productos")
+
+        //VALIDACION QUE TODOS LOS ESPACIOS DE LOS PRDOCUTOS ESTEN LLENOS QUE TENGA TODOS LOS PARAMETROS (la idea es q si falta un parametro no lo pushee en el array de productos)
+        let espaciosVacios = () => {
+            let a = Object.values(producto).includes(undefined)// recorre el objeto producto convetido en array y asi no lo pushea al array principal
+            let bolean = a ? console.log("Hay espacios vacios") : false
+            return bolean
         }
+       
 
         // VALIDACION QUE TODOS LOS PRODUCTOS TENGAN DIFERENTE CODE
-        const codigoRepe = this.products.filter(e => e.code == code)
-        if (codigoRepe.length > 0  ) {
+        let codigoRepe = () => { 
+            let a = this.products.filter(e => e.code == code) 
+            let bolean = (a.length > 0) ? console.log("Hay un codigo repetido") :  false
+            return bolean
+        }
 
-            return console.log("El codigo del nuevo producto esta repetido")
 
-        } else {
+        // CONDICION FINAL PARA QUE LOS PUSHEE AL ARRAY
+        if (espaciosVacios() === false && codigoRepe() === false) {
+
             this.products.push(producto)
-            const productoString = JSON.stringify(this.products)
-            fs.writeFileSync("BD.json", productoString)
-        }
 
+
+        }
 
     }
 
-    getProductById = (id) => {
+    //METODO PARA BUSCAR UN PRODUCTO SEGUN ID
+    getProductById = async (id) => {
+        this.products = await this.getProducts()
+
         const productoFiltrado = this.products.filter(e => e.id == id)
-        if (productoFiltrado.length == 0) {
-            return console.log("Not Found")
+            
+        if (productoFiltrado.length > 0 && typeof id == "number") { //VALIDO CON EL TYPE OF QUE LA ENTRADA DEL PARAMETRO SE SOLO UN NUMERO
+            return console.log ("El producto buscado",productoFiltrado)
         } else {
-            return console.log ("El producto buscado",productoFiltrado)}
+            return console.log("Not Found") }
     }
 
-    deleteProduct = (id)=> {
-        const a = this.products.filter(prod => prod.id !== id)
-        this.products.push(a)
-        const productoString = JSON.stringify(a)
-        fs.writeFileSync("BD.json", productoString)
-        console.log("Profucto eliminado", a )
+
+    deleteProduct = async (id)=> {
+        this.products = await this.getProducts()
+        let a = this.products.filter(prod => prod.id !== id)
+        this.products = a
+        console.log("Producto Eliminado")
     }
 
-    updateProduct = (id, modificarProp)=> {
-        const localizarProducto = async ()=> { 
-            const n = await this.products.find(e => e.id == id)
-            n.title = modificarProp
-            this.products.push(n)
-            const productoString = JSON.stringify(this.products)
-            fs.writeFileSync("BD.json", productoString)
-            console.log("Profucto Modificado", n )
-        }
-        localizarProducto()    
-    }
+    updateProduct = async (id, parametro, actualizacion)=> {
+        this.products = await this.getProducts() 
+        let n = this.products.find(e => e.id == id)
+            
+        switch (parametro) {
+            case "title":
+                n.title = actualizacion
+                    
+                break;
+            case "description":
+                n.description = actualizacion
+                break;
+            case "price":
+                n.price = actualizacion
+                break;
+            case "stock":
+                n.stock = actualizacion
+                break;
+            
+            default:
+                console.log("Parametro Incorrecto")
+                break;
+            }
+        let a = this.products.filter(prod => prod.id !== id)
+        this.products = a
+        this.products.push(n)
+        console.log("Producto Modificado", n )
+    }  
 }
+
 const main = async() => {
     const manager = new ProductManager("BD.json")
-    console.log(await manager.getProducts())
-    manager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123",23)
-    manager.addProduct("1producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc466", 23)
-    manager.addProduct("111111111111 prueba", "Este es un producto prueba", 200, "Sin imagen", "abc4544",25)
-    manager.addProduct("111111111111 prueba", "Este es un producto prueba", 100, "Sin imagen", "abc4544",24)
-    /* manager.deleteProduct(2); */
-    /* manager.updateProduct(3, "Cambio" ) */
+    
+    manager.addProduct("producto prueba 1", "Este es un producto prueba", 200, "Sin imagen", "abc123",23)
+    manager.addProduct("producto prueba 2", "Este es un producto prueba", 200, "Sin imagen", "abc466", 24)
+    manager.addProduct("producto pruena 3", "Este es un producto prueba", 200, "Sin imagen", "abc4545",25)
+    manager.addProduct("producto prueba 4", "Este es un producto prueba", 100, "Sin imagen", "abc4544",24)
+    manager.addProduct("producto prueba 5", "Este es un producto prueba", 100, "Sin imagen", "abc4546",24)
+    manager.addProduct("producto prueba 6", "Este es un producto prueba", 100, "Sin imagen", "abc4547",24)
+    manager.addProduct("producto prueba 7", "Este es un producto prueba", 100, "Sin imagen", "abc4547",24)
+    manager.addProduct("producto prueba 8", "Este es un producto prueba", 100, "Sin imagen", "abc4549",24)
+    manager.addProduct("producto prueba 9", "Este es un producto prueba", 100, "Sin imagen", "abc4550",24)
+    manager.addProduct("producto prueba 10", "Este es un producto prueba", 100, "Sin imagen", "abc4551",24) 
+    console.log(await manager.deleteProduct(3))
+    console.log(await manager.updateProduct(1,"price",21))
+    console.log("Productos",await manager.getProducts())
 }
 
 
